75871ff61e833cdc9e0fefb7e8c20ab66a01aac5
szouga <szouga9@gmail.com>
2026-06-18 15:54:18 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var fullItemList = document.getElementById("my-list");
+fullItemList.addEventListener("click"); // add click
+
+ fullItemList.addEventListener(   ); // add event listener
+/* TODO: Add a variable to store the "my-list" element */
+
+var fullItemList = document.getElementById("my-list");
+fullItemList.addEventListener("click", checkOffItem); // add function name
+/* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
+
+
+/* TODO: Declare the function checkOffList and add actions inside the { } */
+if (fullItemList) {
+  fullItemList.addEventListener("click", checkOffItem);
+}
